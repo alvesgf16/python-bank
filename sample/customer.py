cd91c49e07fe9449ed67c1c320422e4c8b3d6568
alvesgf16 <alvesgabriel1994@gmail.com>
2022-12-21 20:46:09 -0300

    feat: add list of accounts to customer class

--- sample/customer.py
+++ sample/customer.py
@@ -2,6 +2,7 @@ class Customer:
     def __init__(self, name, cpf):
         self.__name = name
         self.__cpf = cpf
+        self.__accounts = []
 
         print(
             f"New customer {self.__name} with CPF {self.__cpf} created!"
@@ -14,3 +15,9 @@ class Customer:
     @property
     def cpf(self):
         return self.__cpf
+
+    def add_account(self, account):
+        self.__accounts.append(account)
+
+    def does_customer_have_an_account_of_type(self, account_type):
+        return account_type in [account.type for account in self.__accounts]
